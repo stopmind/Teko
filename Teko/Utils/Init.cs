deaf1c88a2457c88b50c4dc3737a4f08ac9e0081
stopmind <51329670+stopmind@users.noreply.github.com>
2025-03-30 13:32:42 +0300

    Update Init

--- Teko/Utils/Init.cs
+++ Teko/Utils/Init.cs
@@ -1,4 +1,5 @@
-﻿using Teko.Core;
+﻿using System.Security.Cryptography;
+using Teko.Core;
 using Teko.Graphics;
 using Teko.Registry;
 using Teko.Resources;
@@ -7,15 +8,18 @@ namespace Teko.Utils;
 
 public static class Init
 {
-    public static void Run(uint width, uint height, string title, Scene scene, string[]? packsPaths = null, string logPath = "game.log", AService[]? services = null)
+    public static void Run(uint width, uint height, string title, Scene scene, string[]? packsPaths = null,
+        string? logPath = null, AService[]? services = null, byte[]? key = null, byte[]? iv = null)
     {
         Game.InitWindow(width, height, title);
         
-        Game.AddService(new Logger(logPath));
+        Game.AddService(new Logger(logPath ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "game.log")));
         Game.AddService(new Input.Input());
         Game.AddService(new GraphicsService());
-        Game.AddService(new ResourcesLoader(packsPaths ?? ["Assets"]));
+        Game.AddService(new ResourcesLoader(packsPaths ?? [Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets")]));
         Game.AddService(new RegistryService());
+        Game.AddService(new Storage.Storage(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Storage"), Aes.Create(), 
+            key ?? [0x01, 0x45, 0xaf, 0x01, 0x45, 0xaf], iv ?? [0x01, 0x45, 0xaf, 0x01, 0x45, 0xaf]));
 
         if (services != null)
         {
